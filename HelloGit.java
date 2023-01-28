fbd8661527b81c0117096d50e50a1aba3d9aaf70
Leonardo Lobo (Lio) <81108569+Lcleon@users.noreply.github.com>
2023-01-28 16:39:37 -0300

    Adicionei arquivos via upload

---
+++ HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.lcleon.git;
+
+/**
+ * @author lio
+ * Exemplo de integração do Eclipse com o GIthub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
